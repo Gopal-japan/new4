ac2531beef9dc5d47d7a16bb2e220893c034c8e3
demo project <dahaldahalg2054@gmail.com>
2024-12-03 13:01:53 +0900

    updated

--- test/script.test.js
+++ test/script.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require('../script');
 
 test('add should return the sum of two numbers', () => {
-  expect(add(2, 3)).toBe(4);
+  expect(add(2, 3)).toBe(5);
   expect(add(-1, 1)).toBe(0);
   expect(add(-2, -3)).toBe(-5);
 });
